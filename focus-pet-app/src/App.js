19d3662c9d339243abd1f9ed2f9a7d7b4eb6159f
rafalwodzinski <rafalwodzinski.07@gmail.com>
2026-05-26 10:17:36 +0200

    Dodano routing

--- focus-pet-app/src/App.js
+++ focus-pet-app/src/App.js
@@ -1,34 +1,8 @@
-/*
-  Stary startowy kod Reacta zostawiony celowo jako komentarz.
-  Gdyby trzeba bylo porownac, co bylo na poczatku projektu, jest tutaj:
-
-  import logo from './logo.svg';
-  import './App.css';
-
-  function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-*/
-
+import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Importy komponentów stron
 import FocusSessionPage from './pages/FocusSessionPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -37,22 +11,79 @@ import OnboardingPage from './pages/OnboardingPage';
 import RegisterPage from './pages/RegisterPage';
 import ShopPage from './pages/ShopPage';
 import TasksPage from './pages/TasksPage';
+import NotFoundPage from './pages/NotFoundPage'; // Nowy komponent 404
+
+// Importy związane z autoryzacją
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/session" element={<FocusSessionPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Trasa główna przekierowująca do logowania */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+          {/* Trasy publiczne */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          {/* Trasy chronione */}
+          <Route 
+            path="/home" 
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/tasks" 
+            element={
+              <ProtectedRoute>
+                <TasksPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/shop" 
+            element={
+              <ProtectedRoute>
+                <ShopPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/session" 
+            element={
+              <ProtectedRoute>
+                <FocusSessionPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/onboarding" 
+            element={
+              <ProtectedRoute>
+                <OnboardingPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/logout" 
+            element={
+              <ProtectedRoute>
+                <LogoutPage />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Trasa nieznaleziona (404) */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
